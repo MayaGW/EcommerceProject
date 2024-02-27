ef9d3bfb82d9aa961c9758184c112c022f810af8
Maya Ghamloush <maya.ghamloushe@gmail.com>
2024-02-27 21:48:09 +0100

    Initial Commit

---
+++ Ecommerce/EcommerceApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcommerceApp.swift
+//  Ecommerce
+//
+//  Created by Maya Ghamloush on 27/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct EcommerceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
